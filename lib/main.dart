28f3a0527e264eeb7105b1fd61ade6e0a05fa198
antonioaren <antonioaren2@gmail.com>
2024-08-16 00:55:45 +0200

    refactor: File system structure. Tidy up and improving and refactoring some code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../states/app_state.dart';
-import './screens/favorites.dart';
-import './screens/home.dart';
+import './routes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ShoppingList());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ShoppingList extends StatelessWidget {
+  const ShoppingList({super.key});
 
   // This widget is the root of your application.
   @override
@@ -17,12 +16,12 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => AppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Shopping List',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: const MyHomePage(
+        home: const Routes(
           title: 'Lista de la compra',
         ),
       ),
@@ -32,69 +31,4 @@ class MyApp extends StatelessWidget {
 
 // ...
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var _selectedIndex = 0;
-
-  void _setIndexSelected(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  bool _isMenuLabelsShowUp(double width) {
-    return width >= 600;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final pages = <Widget>[
-      HomePage(),
-      FavoritePage(),
-    ];
-
-    Widget page = pages.elementAt(_selectedIndex);
-
-    // This is a widget that gives us the constrains of the parent widget. We can make things responsive with this.
-    return LayoutBuilder(builder: (context, constrains) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              // This is a widget that makes sure that the content is not behind the status bar or notch.
-              child: NavigationRail(
-                  extended: _isMenuLabelsShowUp(constrains.maxWidth),
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                  ],
-                  selectedIndex:
-                      _selectedIndex, // This is the default selected index
-                  onDestinationSelected: _setIndexSelected),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
---
+++ lib/routes.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import './screens/favorites.dart';
+import './screens/home.dart';
+
+class Routes extends StatefulWidget {
+  const Routes({super.key, required this.title});
+
+  final String title;
+
+  // Create local state for the widget.
+  @override
+  State<Routes> createState() => _Routes();
+}
+
+class PageDestination {
+  final Widget page;
+  final NavigationRailDestination destination;
+
+  PageDestination({
+    required this.page,
+    required this.destination,
+  });
+}
+
+class _Routes extends State<Routes> {
+  // Here is the routes that we can use in the app.
+  var _selectedIndex = 0;
+
+  void _setIndexSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  bool _isMenuLabelsShowUp(double width) {
+    return width >= 600;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pageDestinations = [
+      PageDestination(
+        page: HomePage(),
+        destination: NavigationRailDestination(
+          icon: Icon(Icons.home),
+          label: Text('Home'),
+        ),
+      ),
+      PageDestination(
+        page: FavoritePage(),
+        destination: NavigationRailDestination(
+          icon: Icon(Icons.favorite),
+          label: Text('Favorites'),
+        ),
+      ),
+    ];
+
+    // This is a widget that gives us the constrains of the parent widget. We can make things responsive with this.
+    return LayoutBuilder(builder: (context, constrains) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              // This is a widget that makes sure that the content is not behind the status bar or notch.
+              child: NavigationRail(
+                  extended: _isMenuLabelsShowUp(constrains.maxWidth),
+                  destinations:
+                      pageDestinations.map((e) => e.destination).toList(),
+                  selectedIndex: _selectedIndex,
+                  onDestinationSelected: _setIndexSelected),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: pageDestinations.elementAt(_selectedIndex).page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
