a0610415c019bdc1a57c6d6b95407c2b0d1e1aa7
antonioaren <antonioaren2@gmail.com>
2024-08-16 12:14:42 +0200

    refactor: Routes isolate. Now we have route separate.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_shopping_list_app/presentation/providers/word.dart';
 import 'presentation/providers/counter.dart';
-import 'presentation/routes.dart';
+import 'presentation/widgets/side_menu.dart';
 
 void main() {
   runApp(const ShoppingList());
@@ -27,7 +27,7 @@ class ShoppingList extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: Routes(
+        home: SideMenu(
           title: title,
         ),
       ),
--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -2,17 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/favorites.dart';
 import 'screens/home.dart';
 
-class Routes extends StatefulWidget {
-  const Routes({super.key, required this.title});
-
-  final String title;
-
-  // Create local state for the widget.
-  @override
-  State<Routes> createState() => _Routes();
-}
-
 // Way to store the page and the destination in a single object.
+// We make objects using class.
 class PageDestination {
   final Widget page;
   final NavigationRailDestination destination;
@@ -39,46 +30,3 @@ final pageDestinations = [
     ),
   ),
 ];
-
-class _Routes extends State<Routes> {
-  // Here is the routes that we can use in the app.
-  var _selectedIndex = 0;
-
-  void _setIndexSelected(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  bool _isMenuLabelsShowUp(double width) {
-    return width >= 600;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This is a widget that gives us the constrains of the parent widget. We can make things responsive with this.
-    return LayoutBuilder(builder: (context, constrains) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              // This is a widget that makes sure that the content is not behind the status bar or notch.
-              child: NavigationRail(
-                  extended: _isMenuLabelsShowUp(constrains.maxWidth),
-                  destinations:
-                      pageDestinations.map((e) => e.destination).toList(),
-                  selectedIndex: _selectedIndex,
-                  onDestinationSelected: _setIndexSelected),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: pageDestinations.elementAt(_selectedIndex).page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
